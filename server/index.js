7e9504b4fd394cb7dffcaa399c261b0c38d4220b
Sidharth Satapathy <sidharth.satapathy9@gmail.com>
2018-09-24 17:09:40 +0530

    Added express routes.

--- server/index.js
+++ server/index.js
@@ -30,8 +30,41 @@ pgClient.query('CREATE TABLE IF NOT EXISTS values (number INT)')
 const redis = require('redis')
 const redisClient = redis.createClinet({
     host: keys.redisHost,
-    port: keys.redisPort
+    port: keys.redisPort,
     retry_strategy: () => 1000 
 })
 
 const redisPublisher = redisClient.duplicate();
+
+// Express routes setup
+
+app.get('/', (req, res) => {
+    res.send('Hi')
+});
+
+app.get('/values/all',async (req, res) => {
+    const values = await pgClient.query('SELECT * from values')
+    res.send(values.rows);
+});
+
+app.get('/values/current',async (req, res) => {
+    redisClient.hgetall('values', (err, values) => {
+        res.send(values);
+    })
+});
+
+app.get('/values',async (req, res) => {
+    const index = req.body.index;
+
+     if(pareseInt(index) > 40) {
+        return res.status(422).send('Index value too large')
+     }
+
+    redisClient.hset('values', index, 'Nothing yet!')
+    redisPublisher.publish('insert', index)
+    pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
+
+    res.send({working: true});
+});
+
+app.listen(5000, err => console.log('Listening at 5000'));
