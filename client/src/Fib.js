5ccce3635530c1807ade917efd8e4383cee7b15f
Sidharth Satapathy <sidharth.satapathy9@gmail.com>
2018-09-24 19:50:01 +0530

    Added dom components to render in Fib.js

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -23,4 +23,55 @@ class Fib extends Component {
         const seenIndexes = await axios.get('/api/values/all');
         this.setState({seenIndexes: seenIndexes.data});
     }
+
+    renderSeenIndexes () {
+        return this.state.seenIndexes.map(({ number }) => number).join(', ')
+    }
+
+    renderValues () {
+        const entries = []
+        
+        for (let key in this.state.values) {
+            entries.push(
+                <div key={key}>
+                    For index {key} I calculated {this.sate.values[key]}
+                </div>
+
+            );
+        }
+
+        return entries;
+    }
+
+    handleSubmit = async (event) => {
+        event.preventDefault();
+
+        await axios.post('/api/values', {
+            index: this.state.index
+        });
+        this.setState({ index: '' });
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label>Enter your index:</label>
+                    <input 
+                        value={this.state.index}
+                        onChange={event => this.setState({ index: event.target.value })}
+                    />
+                    <button>Submit</button>
+                </form>
+                <h3> Indexes I have been</h3>
+                {this.renderSeenIndexes()}
+
+                <h3>Calculated Values: </h3>
+                {this.renderValues()}
+            </div>
+
+        );
+    }
 }
+
+export default Fib;
