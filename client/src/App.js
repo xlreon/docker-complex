13b357c1837eeb7b1534feb9fb6fc72b3f45acce
Sidharth Satapathy <sidharth.satapathy9@gmail.com>
2018-09-24 20:32:56 +0530

    Added Links and routes to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import OtherPage from './OtherPage';
+import Fib from './Fib';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      return (
+    <Router>
+        <div> 
+        <header >
+          <Link to="/">Home</Link>
+          <Link to="/otherpage">Other Page</Link>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <div>
+            <Route exact path="/" component={Fib} />
+            <Route path='/otherpage' component={OtherPage} />
+        </div>
+    </div>
+    </Router>
     );
   }
 }
